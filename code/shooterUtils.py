22de7822454d274a8e7c694f81064ea17daaff1d
Karthik <nkartr@gmail.com>
2016-01-02 15:23:46 -0500

    .

---
+++ code/shooterUtils.py
@@ -0,0 +1,28 @@
+''' script to retag the data files, especially for killed and wounded tags'''
+
+import sys, pdb
+
+CONTEXT = 5
+
+cnt = 0
+total = 0
+
+shooterNames = set()
+
+with open(sys.argv[1], 'r') as inFile:
+    with open(sys.argv[2], 'w') as outFile:
+        f = inFile.read().split('\n')
+        for i in range(1, len(f), 2):
+            tokens = f[i].split()
+
+            for j, token in enumerate(tokens):
+                if 'shooterName' in token:
+                    shooterNames.add(token.split('_')[0].lower())
+
+
+        #write the shooter names to file
+        outFile.write(str(list(shooterNames)).replace("'", '"'))
+
+
+
+print "Got ", len(shooterNames), "names."

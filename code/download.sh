93a6b89fcec6c1d7b56f022fa71e5bbc7c047055
Adam Yala <adamyala@mit.edu>
2016-03-16 04:53:31 -0400

    Figured out queries via ngrams, and modified download.py / download script accordingly

--- code/download.sh
+++ code/download.sh
@@ -1,3 +1,10 @@
-for file in "dev" "test" "train"; do 
-    python download.py  ../data/tagged_data/whole_text_full_city2/$file.tag $file.extra;
+i=0
+for extraQuery in "adulterated" "scandal" "countries" "fake"; do
+	i=$((i + 1))
+	for file in "dev" "test" "train"; do 
+    	python download.py  ../data/tagged_data/EMA/$file.tag $file.$i.extra $extraQuery &
+	done;
 done;
+
+wait
+echo "Download complete"
